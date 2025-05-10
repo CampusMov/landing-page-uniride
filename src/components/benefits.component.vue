0530b63848260563bfe5d8de183f532f3e180cd4
Brayan Smith Morales <102981505+Brays83@users.noreply.github.com>
2025-05-10 03:22:30 -0500

    fix: fixed translate in component benefits components

--- src/components/benefits.component.vue
+++ src/components/benefits.component.vue
@@ -5,30 +5,30 @@ const { t } = useI18n();
 
 const benefits = computed(()=>
     [
-      { subtitle1: t("Benefits.Title"),
-        subtitle2: t("Benefits.Comodity"),
-        paragraph: t("Benefits.CDesc"),
-        img: "/mundo.png"
+      { subtitle1: t("benefits.subTitle1"),
+        subtitle2: t("benefits.subTitle2"),
+        paragraph: t("benefits.paragraph1"),
+        img: "https://hiraoka.com.pe/media/mageplaza/blog/post/c/o/como-elegir-el-mejor-celular.jpg"
       },
-      { subtitle1: t("Benefits.Title"),
-        subtitle2: t("Benefits.Comodity"),
-        paragraph: t("Benefits.CDesc"),
-        img: "/calendario.webp"
+      { subtitle1: t("benefits.subTitle3"),
+        subtitle2: t("benefits.subTitle4"),
+        paragraph: t("benefits.paragraph2"),
+        img: "https://www.managers.org.uk/wp-content/uploads/2020/03/TimeSaving-scaled.jpg"
       },
-      { subtitle1: t("Benefits.Title"),
-        subtitle2: t("Benefits.Comodity"),
-        paragraph: t("Benefits.CDesc"),
-        img: "/celular.png"
+      { subtitle1: t("benefits.subTitle5"),
+        subtitle2: t("benefits.subTitle6"),
+        paragraph: t("benefits.paragraph3"),
+        img: "https://latamobility.com/wp-content/uploads/2024/02/HOOP-1.png"
       },
-      { subtitle1: t("Benefits.Title"),
-        subtitle2: t("Benefits.Comodity"),
-        paragraph: t("Benefits.CDesc"),
-        img: "/finanzas.webp"
+      { subtitle1: t("benefits.subTitle7"),
+        subtitle2: t("benefits.subTitle8"),
+        paragraph: t("benefits.paragraph4"),
+        img: "https://easylift.pk/wp-content/uploads/2024/02/carpool-safety.jpeg"
       },
-      { subtitle1: t("Benefits.Title"),
-        subtitle2: t("Benefits.Comodity"),
-        paragraph: t("Benefits.CDesc"),
-        img: "/maleta.png"
+      { subtitle1: t("benefits.subTitle9"),
+        subtitle2: t("benefits.subTitle10"),
+        paragraph: t("benefits.paragraph5"),
+        img: "https://img-cdn.inc.com/image/upload/f_webp,q_auto,c_fit/vip/2024/10/flexible-work-schedules-inc-GettyImages-1468302529.jpg"
       }
     ]
 )
@@ -39,13 +39,13 @@ const benefits = computed(()=>
 <template>
   <section class="flex flex-column p-3 w-full gap-3 ">
     <div class="flex justify-content-center align-content-center">
-      <h2>{{ $t("Benefits.Title") }}</h2>
+      <h2>{{ $t("benefits.title") }}</h2>
     </div>
     <div class="flex flex-column justify-content-center align-items-center w-full">
       <pv-timeline :value="benefits" align="alternate" class="flex flex-column gap-7 w-full md:w-6">
         <template #opposite="slotProps">
           <div class="flex justify-content-center align-items-center h-full">
-            <img class="img-timeline w-12 md:w-auto" :src="slotProps.item.img"/>
+            <img class="img-timeline w-12" :src="slotProps.item.img"/>
           </div>
         </template>
 
@@ -72,5 +72,9 @@ h2{
 }
 p {
   color: #264653;
+}
+img{
+  border-radius: 8px;
+
 }
 </style>
