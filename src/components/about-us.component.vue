d6a2396419efa52c6c25ee43e358759b69d76f7f
Brayan Smith Morales <102981505+Brays83@users.noreply.github.com>
2025-05-10 03:24:50 -0500

    style: fixed style of video

--- src/components/about-us.component.vue
+++ src/components/about-us.component.vue
@@ -10,7 +10,7 @@ const { t } = useI18n();
     <h2>{{ $t("NavBar.About-us") }}</h2>
   </div>
   <div class="flex justify-content-center align-content-center w-full">
-    <iframe class="w-7 h-30rem" src="https://www.youtube.com/embed/iEDbY5mNlKE?si=ssbRy9kp83sfZwvq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <iframe class="md:w-7 md:h-30rem h-15rem" src="https://www.youtube.com/embed/iEDbY5mNlKE?si=ssbRy9kp83sfZwvq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
   </div>
 </section>
 </template>
